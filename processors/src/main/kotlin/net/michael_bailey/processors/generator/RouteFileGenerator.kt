29da2d9d01d36a330911b7975ec6a2b4c86587fe
michael-bailey <bailey-michael1@outlook.com>
2025-10-13 00:14:10 +0100

    Made route generator, use a strategy pattern for route generation.

--- processors/src/main/kotlin/net/michael_bailey/processors/generator/RouteFileGenerator.kt
+++ processors/src/main/kotlin/net/michael_bailey/processors/generator/RouteFileGenerator.kt
@@ -1,82 +1,44 @@
 package net.michael_bailey.processors.generator
 
 import com.google.devtools.ksp.processing.KSPLogger
-import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
-import io.ktor.server.routing.*
 import net.michael_bailey.processors.configuration.ControllerConfiguration
-import net.michael_bailey.processors.configuration.RouteConfiguration
-import net.michael_bailey.processors.strategies.INamingStrategy
-import java.util.Locale.getDefault
+import net.michael_bailey.processors.strategies.handler.IHandlerGenerationStrategy
+import net.michael_bailey.processors.strategies.naming.INamingStrategy
 
 class RouteFileGenerator(
 	private val namingStrategy: INamingStrategy,
+	private val handlerGenerationStrategy: IHandlerGenerationStrategy,
 	private val logger: KSPLogger
 ) {
+
 	fun generate(
 		controllerConfiguration: ControllerConfiguration,
 	): FileSpec {
 
 		logger.info("Generating function for controller routes of: ${controllerConfiguration.qualifiedName}")
 
-		val functionSpecs = transformToFunctionSpecs(controllerConfiguration)
+		val handlerFunctions = controllerConfiguration.routes.map {
+			handlerGenerationStrategy.generateHandler(
+				controllerConfiguration,
+				it
+			)
+		}
 
-		logger.info("Generated ${functionSpecs.size} function specs")
+		logger.info("Generated ${handlerFunctions.size} handler functions specs")
 
-		val fileSpec = FileSpec.builder("io.github.michael_bailey.spring_blog", namingStrategy.getMappingName(controllerConfiguration)).apply {
+		val setupFunction = handlerGenerationStrategy.generateSetup(controllerConfiguration)
+
+		logger.info("Generated setup function")
+
+		val fileSpec = FileSpec.builder("io.github.michael_bailey.spring_blog", namingStrategy.getFilename(controllerConfiguration)).apply {
 
 			addImport("io.ktor.server.routing", "get", "post", "route")
 			addImport("io.ktor.server.response", "respondText")
 
-			functionSpecs.forEach(::addFunction)
+			(handlerFunctions + listOf(setupFunction)).forEach(::addFunction)
 		}.build()
 
 		return fileSpec
 	}
-
-
-	private fun transformToFunctionSpecs(controllerConfiguration: ControllerConfiguration): List<FunSpec> =
-		controllerConfiguration.routes.map { generateFunctionSpecForRoute(controllerConfiguration, it) } + createSetupFunctionForController(
-			controllerConfiguration
-		)
-
-	private fun createSetupFunctionForController(controllerConfiguration: ControllerConfiguration): FunSpec =
-		FunSpec.builder(
-			namingStrategy.getControllerSetupName(controllerConfiguration)
-		).apply {
-			receiver(Routing::class)
-
-			beginControlFlow("route(\"${controllerConfiguration.basePath}\")")
-
-			controllerConfiguration.routes.forEach { route ->
-				addStatement("${
-					namingStrategy.getControllerRouteName(
-						controllerConfiguration,
-						route
-					)
-				}()")
-			}
-
-			endControlFlow()
-		}.build()
-
-	// TODO: Add code to create and controller methods
-	private fun generateFunctionSpecForRoute(controllerConfig: ControllerConfiguration, route: RouteConfiguration): FunSpec =
-		FunSpec.builder(namingStrategy.getControllerRouteName(controllerConfig, route)).apply {
-			receiver(Route::class)
-			CodeBlock.builder().apply {
-				add(
-					"""
-					${route.httpMethod.lowercase(getDefault())}("${route.path}") {
-						val controller = ${route.controllerName}()
-						val res = controller.${route.methodName}()
-						call.respondText(res)
-					}
-				""".trimIndent()
-				)
-
-				build().let(::addCode)
-			}
-		}.build()
 }
