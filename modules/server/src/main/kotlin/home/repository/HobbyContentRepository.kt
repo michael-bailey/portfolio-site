b11fa27534b0796aecc89d5399b5d10669df493f
michael-bailey <bailey-michael1@outlook.com>
2026-08-19 14:47:54 +0100

    Create(feat): Created hobby page content repository

---
+++ modules/server/src/main/kotlin/home/repository/HobbyContentRepository.kt
@@ -0,0 +1,29 @@
+package net.michael_bailey.home.repository
+
+import net.michael_bailey.home.model.ContentArticle
+import net.michael_bailey.home.model.ContentSection
+import org.koin.core.annotation.Single
+
+@Single
+class HobbyContentRepository {
+	fun getContentSections(): List<ContentSection> = listOf(
+		ContentSection(
+			header = "Hobbies",
+			description = "",
+			articles = listOf(
+				ContentArticle(
+					header = "Hello world",
+					paragraphs = listOf(
+						"""Hi there, I'm Michael. I'm a software engineer with a
+							deep interest in user-focused, technology and engineering.
+							I enjoy building tools to assist myself, and others, with my other
+							hobbies and interests.""".trimIndent(),
+						"""This site is a place for me to share projects I'm working on, write
+							about technologies I 'm learning, and put into practice my
+							learnings .""".trimIndent()
+					)
+				)
+			)
+		),
+	)
+}
