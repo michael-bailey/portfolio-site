43c5921fcac9672820221f8c3c3dee7c3234f08a
michael-bailey <bailey-michael1@outlook.com>
2026-08-22 21:57:35 +0100

    delete(feat): Deleted old backend module

--- backend/src/main/kotlin/io/github/michael_bailey/spring_blog/privacy/rules/PrivacyRule.kt
+++
@@ -1,7 +0,0 @@
-package io.github.michael_bailey.spring_blog.privacy.rules
-
-import io.github.michael_bailey.spring_blog.security.viewer.IViewerContext
-
-interface PrivacyRule<TModel> {
-	fun execute(vc: IViewerContext, model: TModel): PrivacyResult
-}
