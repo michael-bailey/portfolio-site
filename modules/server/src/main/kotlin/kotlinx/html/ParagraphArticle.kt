bf93ce108f92f859064fcb7859c0388f9354a467
michael-bailey <bailey-michael1@outlook.com>
2026-08-25 20:09:23 +0100

    create(feat): Created paragraph article, from CustomArticle, to handle typing better

---
+++ modules/server/src/main/kotlin/kotlinx/html/ParagraphArticle.kt
@@ -0,0 +1,34 @@
+package net.michael_bailey.kotlinx.html
+
+import kotlinx.html.*
+
+class ParagraphArticle(
+	initialAttributes: Map<String, String>,
+	override val consumer: TagConsumer<*>
+): HTMLTag("article", consumer, initialAttributes, null, false, false), ArticleContent, HtmlBlockTag, FlowOrPhrasingContent {
+
+	private val components: MutableList<ParagraphArticle.() -> Unit> = mutableListOf()
+
+	override var header: String = ""
+
+	override fun para(block: P.() -> Unit) {
+		components += {
+			p(block = block)
+		}
+	}
+
+	override fun customContent(block: (FlowOrPhrasingContent).() -> Unit) {
+		components += block
+	}
+
+	fun render() {
+		visit {
+			h3 {
+				+this@visit.header
+			}
+			components.forEach {
+				it()
+			}
+		}
+	}
+}
