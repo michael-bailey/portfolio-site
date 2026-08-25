7b0690a2d99772bf64932a0ad123c47138f2c019
michael-bailey <bailey-michael1@outlook.com>
2026-08-25 20:31:49 +0100

    create(feat): Created new main component class using new setup

--- modules/server/src/main/kotlin/kotlinx/custom_main.kt
+++ modules/server/src/main/kotlin/kotlinx/custom_main.kt
@@ -3,11 +3,11 @@ package net.michael_bailey.kotlinx
 import kotlinx.html.BODY
 import kotlinx.html.emptyMap
 import kotlinx.html.visit
-import net.michael_bailey.kotlinx.html.CustomMain
 import net.michael_bailey.kotlinx.html.Main
+import net.michael_bailey.kotlinx.html.SectionContainer
 
 inline fun BODY.main(
-	crossinline block: Main.() -> Unit = {}
+	crossinline block: SectionContainer.() -> Unit = {}
 ): Unit {
-	CustomMain(emptyMap, consumer).visit(block)
+	Main(emptyMap, consumer).visit(block)
 }
--- modules/server/src/main/kotlin/kotlinx/html/Main.kt
+++ modules/server/src/main/kotlin/kotlinx/html/Main.kt
@@ -1,5 +1,25 @@
 package net.michael_bailey.kotlinx.html
 
-interface Main {
-	fun section(header: String, block: Section.() -> Unit)
+import kotlinx.html.*
+
+open class Main(
+	initialAttributes: Map<String, String>,
+	override val consumer: TagConsumer<*>
+) : HTMLTag(
+	tagName = "main",
+	consumer = consumer,
+	initialAttributes = initialAttributes,
+	namespace = null,
+	inlineTag = false,
+	emptyTag = false
+), SectionContainer, HtmlBlockTag, FlowOrPhrasingContent {
+
+	override fun basicSection(block: SectionContent.() -> Unit) {
+		Section(
+			initialAttributes = emptyMap,
+			consumer = consumer
+		).apply(block).render()
+	}
+
+
 }
