aad66bd4daa63a5d4975ea2c7c1800974bcabf35
Michael Bailey <bailey-michael1@outlook.com>
2026-08-27 19:59:52 +0100

    Created Navigation component (#86)
    Signed-off-by: michael-bailey <bailey-michael1@outlook.com>

---
+++ modules/server/src/main/kotlin/components/NavigationContainer.kt
@@ -0,0 +1,6 @@
+package net.michael_bailey.components
+
+interface NavigationContainer {
+	fun addLink(href: String, text: String)
+	fun addDropdown()
+}
--- modules/server/src/main/kotlin/kotlinx/custom_main.kt
+++ modules/server/src/main/kotlin/kotlinx/custom_main.kt
@@ -2,6 +2,8 @@ package net.michael_bailey.kotlinx
 
 import kotlinx.html.BODY
 import kotlinx.html.emptyMap
+import net.michael_bailey.components.Navigation
+import net.michael_bailey.components.NavigationContainer
 import net.michael_bailey.kotlinx.html.Main
 import net.michael_bailey.kotlinx.html.SectionContainer
 
@@ -11,4 +13,10 @@ inline fun BODY.main(
 	Main(emptyMap, consumer)
 		.apply(block)
 		.render()
+}
+
+inline fun BODY.nav(
+	crossinline block: NavigationContainer.() -> Unit = {},
+) {
+	Navigation(this.consumer).apply(block).render()
 }
