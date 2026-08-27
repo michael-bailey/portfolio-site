982251f7eeb653589ee0cf7dbe5a6c0108809a02
michael-bailey <bailey-michael1@outlook.com>
2026-08-27 11:57:10 +0100

    update(feat): Converted section to HtmlComponent
    Signed-off-by: michael-bailey <bailey-michael1@outlook.com>

--- modules/server/src/main/kotlin/kotlinx/html/Section.kt
+++ modules/server/src/main/kotlin/kotlinx/html/Section.kt
@@ -1,12 +1,14 @@
 package net.michael_bailey.kotlinx.html
 
 import kotlinx.html.*
+import net.michael_bailey.components.HtmlComponent
 
 class Section(
 	initialAttributes: Map<String, String>,
-	override val consumer: TagConsumer<*>
-): HTMLTag("section", consumer, initialAttributes, null, false, false),
-	SectionContent, HtmlBlockTag, FlowOrPhrasingContent {
+	override val consumer: TagConsumer<*>,
+) : HtmlComponent(
+	tagName = "section", consumer, initialAttributes, null, false, false
+), SectionContent, HtmlBlockTag, FlowOrPhrasingContent {
 
 	private var components = mutableListOf<Section.() -> Unit>()
 
@@ -15,14 +17,12 @@ class Section(
 	override fun paragraphArticle(block: ParagraphArticle.() -> Unit) {
 		components += {
 			ParagraphArticle(
-				initialAttributes = emptyMap,
-				consumer = consumer
+				initialAttributes = emptyMap, consumer = consumer
 			).apply(block).render()
 		}
 	}
 
-
-	fun render() = visit {
+	override fun renderContents() {
 		h2 {
 			+this@Section.header
 		}
