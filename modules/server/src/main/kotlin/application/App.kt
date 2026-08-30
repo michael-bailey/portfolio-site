c892035ede0b93cabc5697359df3d25c660dd6d9
michael-bailey <bailey-michael1@outlook.com>
2026-08-30 10:43:22 +0100

    create(feat): Modified app setup to use new functions
    Signed-off-by: michael-bailey <bailey-michael1@outlook.com>

--- modules/server/src/main/kotlin/application/App.kt
+++ modules/server/src/main/kotlin/application/App.kt
@@ -1,17 +1,15 @@
 package net.michael_bailey.application
 
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.metrics.micrometer.*
-import io.ktor.server.plugins.callid.*
 import io.ktor.server.routing.*
+import net.michael_bailey.application.plugins.setupCallId
+import net.michael_bailey.application.plugins.setupMetrics
 import net.michael_bailey.home.controller.HomeController.Companion.setupHome
 import net.michael_bailey.observability.setupHealthController
 import org.koin.core.annotation.KoinApplication
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 import org.koin.plugin.module.dsl.withConfiguration
-import kotlin.uuid.Uuid
 
 @KoinApplication(modules = [
 	AppModule::class
@@ -24,19 +22,8 @@ object App {
 			withConfiguration<App>()
 		}
 
-		install(MicrometerMetrics) {
-			this.registry = registry
-		}
-
-		install(CallId) {
-			header(HttpHeaders.XRequestId)
-			verify { callId: String ->
-				callId.isNotEmpty()
-			}
-			this.generate {
-				Uuid.generateV7().toString()
-			}
-		}
+		setupCallId()
+		setupMetrics()
 
 		routing {
 			setupHealthController()
