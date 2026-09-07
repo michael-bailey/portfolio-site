4c8a871aa39ad9ac4c196f4670ff34e121f1a635
michael-bailey <bailey-michael1@outlook.com>
2026-09-07 21:24:11 +0100

    update(feat): Reverted change to port numbers

--- modules/server/src/main/kotlin/main.kt
+++ modules/server/src/main/kotlin/main.kt
@@ -5,7 +5,7 @@ import io.ktor.server.netty.*
 import net.michael_bailey.application.App.setup
 
 fun main() {
-	embeddedServer(Netty, port = 8000) {
+	embeddedServer(Netty, port = 8080) {
 		this.setup()
 	}.start(true)
 }
